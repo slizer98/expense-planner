38a62fb414609a7f9888f28d45a6e1413e858744
slizer98 <erick.m.villa.r@gmail.com>
2024-04-14 12:01:45 -0600

    Add ErrorMessage component for displaying error messages

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+type ErrorMessageProps = {
+  children: ReactNode
+}
+
+export default function ErrorMessage({children}:ErrorMessageProps) {
+  return (
+    <p className="bg-red-600 p-2 text-white font-bold text-sm text-center">{children}</p>
+  )
+}
